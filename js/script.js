5530fa67ef440e6c4ba3b30f527de0a7b9edc9b3
DodoBey <dogukanyigiter@gmail.com>
2021-12-22 22:42:15 -0800

    final revised 2

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onscroll = function () {
 
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarNavDropdown')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
